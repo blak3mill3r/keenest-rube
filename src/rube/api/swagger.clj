c367e59bf37d6502c39feaa9c04c5ce3ef5b22f1
Gardner Vickers <gardner@vickers.me>
2017-11-21 10:02:46 -0500

    Add logging, drop unecessary threads, ensure cleanup on termination.

--- src/rube/api/swagger.clj
+++ src/rube/api/swagger.clj
@@ -1,6 +1,7 @@
 (ns rube.api.swagger
   (:require [clojure.string :as str]
             [clojure.java.io :as io]
+            [taoensso.timbre :as timbre]
             [aleph.http :as http]
             [byte-streams :as bs]
             [manifold.deferred :as md]
@@ -10,24 +11,25 @@
 (defn gen-resource-map [server]
   "Queries Kubernetes for watchable resources, returning a map of resource name
   to URI path suitable for get operations."
-    (-> (md/chain (http/get (str server "/swagger.json")
-                             {:accept "application/json"})
-                   (fn [resp]
-                     (let [body (bs/to-string (:body resp))
-                           paths (get (json/read-str body) "paths")]
-                       (into {}
-                             (comp
-                              (filter (fn [[k v]]
-                                        (and (re-matches #".*watch.*" k)
-                                             (re-matches #".*namespace.*" k)
-                                             (not (re-matches #".*\{name\}.*" k)))))
-                              (map (fn [[k v]]
-                                     [(last (str/split k #"/"))
-                                      (str/replace k #"/watch" "")])))
-                             paths))))
-         (md/catch Exception
-             (fn [e]
-               (throw (ex-info "Could not connect to Kubernetes API server swagger server" {:status 404}))))))
+  (timbre/info "Fetching Kubernetes API layout from swagger endpoint")
+  (-> (md/chain (http/get (str server "/swagger.json")
+                          {:accept "application/json"})
+                (fn [resp]
+                  (let [body (bs/to-string (:body resp))
+                        paths (get (json/read-str body) "paths")]
+                    (into {}
+                          (comp
+                           (filter (fn [[k v]]
+                                     (and (re-matches #".*watch.*" k)
+                                          (re-matches #".*namespace.*" k)
+                                          (not (re-matches #".*\{name\}.*" k)))))
+                           (map (fn [[k v]]
+                                  [(last (str/split k #"/"))
+                                   (str/replace k #"/watch" "")])))
+                          paths))))
+      (md/catch Exception
+          (fn [e]
+            (throw (ex-info "Could not connect to Kubernetes API server swagger server" {:status 404}))))))
 
 (defn path-pattern
   "Return a resource path URI for a resource, pre-templated with `{namespace}`"
--- src/rube/core.clj
+++ src/rube/core.clj
@@ -1,6 +1,6 @@
 (ns rube.core
   (:require [clojure.string :as str]
-            [clojure.core.async :as a :refer [<!! timeout]]
+            [clojure.core.async :as a :refer [<!! timeout <! go-loop]]
             [taoensso.timbre :as timbre]
             [rube.lens :refer [resource-lens]]
             [rube.api.swagger :as api]
@@ -19,8 +19,9 @@
           supported-resources (set (keys resource-map))
           kill-ch (a/chan) ;; Used for canceling in-flight requests
           kube-atom (atom {:context this})]
-      (timbre/info "Starting Kubernetes")
+      (timbre/info "Starting Kubernetes Informer")
       (doseq [resource-name supported-resources]
+        (timbre/debug "Initializing watch for: " resource-name)
         (watch-init! kube-atom resource-map resource-name kill-ch))
       (assoc this
              ::api/resource-map resource-map
@@ -31,7 +32,9 @@
                          @kube-atom)
              ::kill-ch kill-ch)))
   (stop [this]
+    (timbre/info "Stopping Kubernetes Informer")
     (when-let [kc (get this ::kill-ch)]
+      (timbre/info "Stopping in-flight Kubernetes Informer requests")
       (a/close! kc))
     (dissoc this ::api/resource-map)))
 
@@ -39,24 +42,17 @@
   [v ^java.io.Writer w]
   (.write w "<KubernetesCluster>"))
 
-(defn new-kubernetes-informer [{:keys [server username password whitelist namespace] :as opts}]
+(defn new-kubernetes-informer
+  "Create a Kubernetes informer component, allowing for subscription and
+  manipulation of Kubernetes resources.
+
+  Returns a KubernetesInformer:
+
+  :rube.core/kube-atom - atom containing a live-updating view of cluster state.
+  :rube.core/informer - map of resource types to atoms allowing swap! and reset!
+                        operations to modify cluster state."
+  [{:keys [server namespace username password whitelist] :as opts}]
   (map->KubernetesInformer opts))
-
-(defn disconnect!
-  "Tear down a kube atom"
-  [kube-atom]
-  (if-let [ch (-> @kube-atom :kill-ch)]
-    (do (println "Closing kill chan") (a/close! ch))
-    (println "Hmm, there's no kill chan?"))
-  (reset! kube-atom {:state :disconnected}))
-
-(defn context
-  "Helper function to create a kube context"
-  [server namespace & {:keys [username password]}]
-  {:server server
-   :namespace namespace
-   :username username
-   :password password})
 
 (defn update-from-snapshot
   "Incorporate `items`, a snapshot of the existing set of resources of a given kind."
@@ -70,7 +66,7 @@
 (defn update-from-event
   "Map from k8s watch-stream messages -> state transition functions for the kube atom."
   [resource-name name type object]
-  (println "Updating " resource-name name type)
+  (timbre/infof "Applying %s %s update event to resource %s" resource-name type name)
   (condp get type
     #{ "DELETED" }          #(update % (keyword resource-name) dissoc name)
     #{ "ADDED" "MODIFIED" } #(update % (keyword resource-name) assoc name object)
@@ -80,24 +76,27 @@
   "Called when the watch channel closes (network problem?)."
   [kube-atom resource-map resource-name kill-ch & {:keys [max-wait] :or {max-wait 8000}}]
   (loop [wait 500]
+    (timbre/debug "Attempting to reconnect...")
     (or (get #{:connected} (watch-init! kube-atom resource-map resource-name kill-ch))
         (do (<!! (timeout wait))
             (if (> wait max-wait) :gave-up (recur (* 2 wait)))))))
 
 (defn- state-watch-loop! [kube-atom resource-map resource-name watch-ch kill-ch]
-  (a/thread
-    (loop []
-      (let [msg (<!! watch-ch)
+  (go-loop [msg (<! watch-ch)
             type (get msg "type")
             object (get msg "object")]
-        (case msg nil (case (reconnect-or-bust kube-atom resource-map resource-name kill-ch)
-                        :connected (println "Reconnected!")
-                        :gave-up   (println "Gave up!"))
-              (do
-                (swap! kube-atom (update-from-event resource-name (get-in object ["metadata" "name"])
-                                                    type
-                                                    object))
-                (recur)))))))
+    (timbre/debug "State update recieved")
+    (if msg
+      (do
+        (swap! kube-atom (update-from-event resource-name (get-in object ["metadata" "name"])
+                                            type
+                                            object))
+        (recur (<! watch-ch)
+               (get msg "type")
+               (get msg "object")))
+      (case (reconnect-or-bust kube-atom resource-map resource-name kill-ch)
+        :connected (timbre/info "Kubernetes Informer reconnected.")
+        :gave-up   (timbre/error "Kubernetes Informer gave up trying to reconnect.")))))
 
 (defn- state-watch-loop-init!
   "Start updating the state with a k8s watch stream. `body` is the response with the current list of items and the `resourceVersion`."
@@ -128,11 +127,14 @@
           (state-watch-loop-init! kube-atom resource-map resource-name body kill-ch)
           response)))
 
+(comment (let [s (component/start (map->KubernetesInformer {:server "http://127.0.0.1:8001"
 
-;; (def rl (resource-lens (::kube-atom s) (::api/resource-map s) :jobs))
+                                                            :namespace "default"
+                                                            :whitelist #{"namespaces"}
 
-;; (= (let [v (str (rand-int 1000))]
-;;      (swap! rl assoc-in ["pyroclast-admin" "metadata" "labels" "foo"] v)
-;;      v)
-;;    (get-in @rl ["pyroclast-admin" "metadata" "labels" "foo"]))
-;; ;;(component/stop s)
+                                                            }))]
+           (swap! (:namespaces (::informer s)) assoc "newns" {"metadata" {"name" "newns"}})
+           (Thread/sleep 1000)
+           (swap! (:namespaces (::informer s)) dissoc "newns")
+           (Thread/sleep 1000)
+           (component/stop s)))
